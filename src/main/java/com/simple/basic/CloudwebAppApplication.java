d87467edd4b9808d971a7ad044ca14eb90babee8
0EH <57349837+0EH@users.noreply.github.com>
2022-10-04 12:12:13 +0900

    ww

---
+++ src/main/java/com/simple/basic/CloudwebAppApplication.java
@@ -0,0 +1,13 @@
+package com.simple.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudwebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudwebAppApplication.class, args);
+	}
+
+}
